ae3161bab2338bef602e43315b62baeb8f5c45b7
itso-wavy <you.meet.vvavy@gmail.com>
2023-06-24 19:45:28 +0900

    twcss 5.react component

--- tailwindCSS/src/App.jsx
+++ tailwindCSS/src/App.jsx
@@ -1,13 +1,14 @@
-import './css/tailwind.css';
 import logoImg from './assets/logo.svg';
 import bgImg from './assets/beach-work.jpg';
+import popularDestinations from './data/popularDestinations';
+import DestinationCard from './components/DestinationCard';
 
-const App = () => {
+export default function App() {
   return (
-    <section id='header' className='min-h-screen bg-gray-100'>
+    <div>
       <div className='bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5'>
         <div className='px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2'>
-          <div class='xl:max-w-xl'>
+          <div className='xl:max-w-xl'>
             <img className='h-10' src={logoImg} alt='Workcation' />
             <img
               className='mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden'
@@ -16,7 +17,7 @@ const App = () => {
             />
             <h1 className='mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl'>
               You can work from anywhere.
-              <br class='hidden lg:inline' />
+              <br className='hidden lg:inline' />{' '}
               <span className='text-indigo-500'>Take advantage of it.</span>
             </h1>
             <p className='mt-2 text-gray-600 sm:mt-4 sm:text-xl'>
@@ -24,29 +25,37 @@ const App = () => {
               locations so you can enjoy some nice weather even when you're not
               on vacation.
             </p>
-            <div className='mt-4 space-x-2 sm:mt-6'>
+            <div className='mt-4 space-x-1 sm:mt-6'>
               <a
+                className='inline-block px-5 py-3 rounded-lg transform transition bg-indigo-500 hover:bg-indigo-400 hover:-translate-y-0.5 focus:ring-indigo-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base'
                 href='/'
-                className='btn btn-primary transform transition hover:-translate-y-0.5 shadow-lg'
               >
                 Book your escape
-              </a>
-              <a class='btn btn-secondary' href='/'>
-                Learn more
               </a>
             </div>
           </div>
         </div>
-        <div class='hidden relative lg:block 2xl:col-span-3'>
+        <div className='hidden relative lg:block 2xl:col-span-3'>
           <img
-            class='absolute inset-0 w-full h-full object-cover object-center'
+            className='absolute inset-0 w-full h-full object-cover object-center'
             src={bgImg}
             alt='Woman workcationing on the beach'
           />
         </div>
       </div>
-    </section>
-  );
-};
 
-export default App;
+      <div className='max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8'>
+        <h2 className='text-xl text-gray-900'>Popular destinations</h2>
+        <p className='mt-2 text-gray-600'>
+          A selection of great work-friendly cities with lots to see and
+          explore.
+        </p>
+        <div className='mt-6 grid gap-6 lg:grid-cols-2 xl:grid-cols-3'>
+          {popularDestinations.map(destination => (
+            <DestinationCard destination={destination} key={destination.city} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwindCSS/src/index.js
+++ tailwindCSS/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './css/tailwind.css';
+
+document.querySelector('body').className = 'bg-gray-200';
 
 createRoot(document.getElementById('root')).render(<App />);
